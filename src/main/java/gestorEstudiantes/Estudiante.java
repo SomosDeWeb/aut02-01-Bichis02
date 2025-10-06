557573417e2cb94e08e33469ff61bb00d9eb10da
Jacqueline <bichispitas@gmail.com>
2025-10-06 19:28:14 +0100

    Clase Estudiante completa

--- src/main/java/gestorEstudiantes/Estudiante.java
+++ src/main/java/gestorEstudiantes/Estudiante.java
@@ -1,25 +1,72 @@
 package gestorEstudiantes;
 
 public class Estudiante {
-	// Propiedades
+	
+	// Atributos
 	private String nombre;
+	private int edad;
 	private double nota;
+	private boolean matriculado;
 	
 	// Constructor. Crea un estudiante con su nombre y nota
-	public Estudiante(String nombre, double nota) {
-		this.nombre = nombre;
-		this.nota = nota;
+	public Estudiante(String nombre, int edad, double nota, boolean matriculado) {
+		this.setNombre(nombre);
+		this.setEdad(edad);
+		this.setNota(nota);
+		this.setMatriculado(matriculado);
 	}
 	
-	// Los Getters son funciones que permiten leer los datos del estudiante
+	// Getters de la clase Estudiante. Los Getters son funciones que permiten leer los datos del estudiante
 	public String getNombre() {
-		return nombre;
+		return this.nombre.toUpperCase();
+	}
+	
+	public int getEdad() {
+		return this.edad;
 	}
 	
 	public double getNota() {
-		return nota;
+		return this.nota;
+	}
+	
+	public boolean getMatriculado() {
+		return this.matriculado;
+	}
+
+
+	// Setters de la clase Estudiante. Modifica los atributos y valida los datos
+	
+	public void setNombre(String nombre) {
+		if (nombre != null && nombre.length() > 0) {
+			if (nombre.matches("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+")) {
+				this.nombre = nombre.trim(); 
+			} 
+		} 
+	}
+	
+	public void setEdad(int edad) {
+		if (edad >= 0 && edad <= 110) {
+			this.edad = edad;
+		} 
+	}
+	
+	public void setNota(double nota) {
+		if (nota >= 0 && nota <= 10) {
+			this.nota = nota;
+		} 
+	}
+	
+	public void setMatriculado (boolean matriculado) {
+		this.matriculado = matriculado;
+	}
+	
+	// Método para mostrar los datos
+	
+	public String mostrarDatos() {
+		return this.nombre + " Edad: " + this.edad + " Nota media: " + this.nota + " Matriculado: " + this.matriculado;
 	}
 }
+
 
 
    
